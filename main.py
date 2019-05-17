f799beb58da0217fd4a43c1f5b789c88d4681480
Josh Miller <jmiller@colgate.edu>
2019-05-16 21:21:36 -0400

    wip tkinter update

--- main.py
+++ main.py
@@ -23,38 +23,49 @@ import tkinter as tk
 # from kivy.core.window import Window
 
 MSG_QUEUE = []
+MAX_HISTORY = 12
 
 class GUI(tk.Frame):
 
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
-		self.pack()
 		self.create_widgets()
 		read_thread = Thread(target=self.read)
 		read_thread.start()
 
 	def create_widgets(self):
-		self.output = tk.Label(self)
-		self.output.configure(text="Init", justify="left")
-		self.output.pack(side="top")
+		self.txt = tk.Text(self.master,borderwidth=3, relief="sunken")
+		self.txt.config(font=("consolas",12), wrap='word',state=DISABLED)
+		self.txt.grid(row=0,column=0,sticky="nsew",padx=2,pady=2)
+		self.scroll = tk.Scrollbar(self.master, command=self.txt.yview)
+		self.scroll.grid(row=0, column=1, sticky="nwes")
+		self.txt['yscrollcommand'] = self.scroll.set
 
-	def write(self, message):
-		new_msg = self.output.cget("text") + "\n" + message
-		self.output.configure(text=new_msg)
-		
 	def read(self):
-		while (len(MSG_QUEUE)):
-			msg = MSG_QUEUE.pop()
-			self.write(msg)
-		time.sleep(1)
+		global MSG_QUEUE
+		MSG_QUEUE = MSG_QUEUE[-MAX_HISTORY:]
+		print("MSG_QUEUE: ")
+		print(MSG_QUEUE)
+		new_msg = ""
+		while len(MSG_QUEUE):
+			self.txt.insert(END, MSG_QUEUE.pop())
+			self.txt.insert(END, '\n')
+		time.sleep(0.5)
 		self.read()
+		
 		
 def run_gui():
 	root = tk.Tk()
-	app = GUI(master=root)
+	root.protocol("WM_DELETE_WINDOW", root.quit)
+	frame = tk.Frame(root, width=600, height=400)
+	frame.pack(fill="both", expand=True)
+	frame.grid_propagate(False)
+	frame.grid_rowconfigure(0, weight=1)
+	frame.grid_columnconfigure(0, weight=1)
+	app = GUI(master=frame)
 	agent_thread = Thread(target=run_agent)
-	agent_thread.start()
+	#agent_thread.start()
 	app.mainloop()
 		
 def run_agent():
