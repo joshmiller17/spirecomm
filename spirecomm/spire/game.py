c07a390077870471b62610128d9fd712c238f2af
Josh Miller <jmiller@colgate.edu>
2019-07-31 09:34:13 -0400

    itemize mcts reward

--- spirecomm/spire/game.py
+++ spirecomm/spire/game.py
@@ -32,6 +32,36 @@ class RoomPhase(Enum):
 	EVENT = 2,
 	COMPLETE = 3,
 	INCOMPLETE = 4
+	
+
+class Reward:
+	
+	def __init__(self, reward={}):
+		self.totalItemized = reward
+		
+	def __str__(self):
+		ret = ", ".join([key + ": " + str(value) for key, value in self.totalItemized.items()])
+		return ret
+		
+	def addReward(self, reward):
+		if reward is None:
+			return self
+		for key, value in reward.totalItemized.items():
+			if key in self.totalItemized:
+				self.totalItemized[key] += value
+			else:
+				self.totalItemized[key] = value
+		return self
+		
+	def getTotalReward(self):
+		ret = 0
+		for key, value in self.totalItemized.items():
+			ret += value
+		return ret
+		
+	def getTotalItemized(self):
+		return self.totalItemized
+	
 
 
 class Game:
@@ -292,18 +322,26 @@ class Game:
 			
 		delta_hp = self.player.current_hp - original_game_state.player.current_hp
 		delta_max_hp = self.player.max_hp - original_game_state.player.max_hp
-		delta_potions = len(self.potions) - len(original_game_state.potions)
+		orig_potions = 0
+		for p in original_game_state.potions:
+			if p.name != "Potion Slot":
+				orig_potions += 1
+		delta_potions = -1 * orig_potions
+		for p in original_game_state.potions:
+			if p.name != "Potion Slot":
+				delta_potions += 1
 		
-		reward = 0
-		reward += delta_hp * MCTS_HP_VALUE
-		reward += delta_max_hp * MCTS_MAX_HP_VALUE
-		reward += delta_potions * MCTS_POTION_VALUE
-		reward -= self.combat_round * MCTS_ROUND_COST
+		r = {}
+		r["HP"] = delta_hp * MCTS_HP_VALUE
+		r["max HP"] = delta_max_hp * MCTS_MAX_HP_VALUE
+		#r["potions"] = delta_potions * MCTS_POTION_VALUE
+		#r -= self.combat_round * MCTS_ROUND_COST
+		reward = Reward(r)
 		
 		if self.debug_file:
 			with open(self.debug_file, 'a+') as d:
 				d.write("\n~~~~~~~~~~~~~~\n")
-				d.write("\nTerminal state reached, reward: " + str(reward) + "\n")
+				d.write("\nTerminal state reached, reward: " + str(reward.getTotalItemized()) + "\n")
 				d.write(str(self))
 				d.write("\n~~~~~~~~~~~~~~\n")
 		
