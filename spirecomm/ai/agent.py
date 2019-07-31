58f91fc233313df93316c8d3bdcd778b58e456d3
Josh Miller <jmiller@colgate.edu>
2019-07-31 17:40:18 -0400

    wip #27

--- spirecomm/ai/agent.py
+++ spirecomm/ai/agent.py
@@ -429,7 +429,8 @@ class SimpleAgent:
 						
 			
 			if monster_changes != {}:
-				diff["monsters"] = monster_changes
+				for key, value in monster_changes.items():
+					diff[key] = value
 			
 			# general fixme?: better record linking between state1 and state2? right now most record linking is by name or ID (which might not be the same necessarily)
 			
