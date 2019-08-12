73b3337854c358d83bc6fea7549c40a2170d9d96
Josh Miller <jmiller@colgate.edu>
2019-08-12 08:30:13 -0400

    init simulator

---
+++ spirecomm/ai/simulator.py
@@ -0,0 +1,20 @@
+from enum import Enum
+import copy
+import random
+import math
+
+
+import spirecomm.spire.relic
+import spirecomm.spire.card
+import spirecomm.spire.character
+import spirecomm.spire.map
+import spirecomm.spire.potion
+import spirecomm.spire.screen
+import spirecomm.spire.power
+
+from spirecomm.communication.action import *
+
+class Simulator:
+
+	def __init__(self):
+		pass
